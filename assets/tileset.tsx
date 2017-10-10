f1ffeb81544d3b88d5c59efd70fdf2e819cb061e
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2017-10-10 22:13:14 +0100

    added tiled map + tileset

---
+++ assets/tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tileset" tilewidth="8" tileheight="8" tilecount="256" columns="16">
+ <image source="tileset.png" width="128" height="128"/>
+</tileset>
